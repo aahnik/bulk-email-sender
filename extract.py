154e52b8d781ba0860a4d71ceda51cdcc6bf3b28
aahnik <aahnikdaw@gmail.com>
2020-06-21 22:44:06 +0530

    final extract

--- extract.py
+++ extract.py
@@ -1,2 +1,39 @@
+# this function extracts data from config.yaml
+import yaml
+import smtplib
+
+
+configs = {}
+
+
 def extract():
-    pass
+    with open('config.yaml', 'r') as file:
+        global configs
+        configs = yaml.full_load(file)
+
+
+def checkConnection():
+    flag = True
+    print("\n\nCHECKING CONNECTION...")
+    try:
+        server = smtplib.SMTP('smtp.gmail.com: 587')
+        server.starttls()
+        server.login(configs['user_name'], configs['auth_code'])
+    except Exception as e:
+        flag = False
+        error = str(e)
+        if ("Username and Password not accepted" in error):
+            print("\n   Incorrect Login Credentials :-( \n")
+        else:
+            print("\nSOME UNKNOWN ERROR OCCURED :-( SEE DETAILS BELOW \n\n", e)
+    finally:
+        return flag
+
+
+if __name__ == "__main__":
+    print('\n\n\n')
+    extract()
+    print(configs)
+    print("CONNECTION:", "\tSUCCESS" if checkConnection() == True else "\tFAILED")
+    print('\n\n\n')
+
--- text_to_html.py
+++
@@ -1,3 +0,0 @@
-def convert(txt_file):
-    pass
-
